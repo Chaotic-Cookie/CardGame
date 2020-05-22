4bc467cc2d56ec7dd420c3a202bb40a9fe358fd1
mangamariam <mangamariam2001@gmail.com>
2020-05-21 23:24:54 -0700

    something or another

--- CardGame.java
+++ CardGame.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 
@@ -10,8 +11,9 @@ Lab 4: Deck of cards
 This program will play a game of 21/black jack
 */
 
-public class Card_Game{
-    
+public class CardGame{
+
+    public static Scanner note = new Scanner(System.in);
 
     public static void main(String[] args) {
     int menu = 0;
@@ -20,7 +22,7 @@ public class Card_Game{
         System.out.println("1. How to play");
         System.out.println("2. Lets Play.");
         System.out.println("3. Quit");
-        String response = book.nextLine();
+        String response = note.nextLine();
         menu = Integer.parseInt(response);
 
         switch (menu) {
@@ -42,9 +44,9 @@ public class Card_Game{
                 }//end switch case
             }//end while loop
         }//end main
-
+/*
     // will deal the original 2 cards
-    public dealHand(){
+    public static void game(){
         //randomizer needed to deal the cards fairly
 
     }//end dealing hand
@@ -67,6 +69,7 @@ public class Card_Game{
         }
             
     }//end compare
+    */
     private static void instuctions() {
         //instructions on how to play the game
         System.out.println("Aim of the 21 Card Game is to get 21 or as close to as possible.");
@@ -79,11 +82,12 @@ public class Card_Game{
         System.out.println("The player needs to decide if they want another card (hit) or will sit on what they have.");
         System.out.println("You can have as many cards as you like as long as you don’t go over 21.");
         System.out.println("Players are not competing against each other, but against the dealer.");
-        System.out.println("The dealer then turns over their other card and needs to decide what to do. If the dealer has 16 or under then they must take another card.");
+        System.out.println("The dealer then turns over their other card and needs to decide what to do.");
+        System.out.println("If the dealer has 16 or under then they must take another card.");
         System.out.println("If the dealer has 21 (Ace and a ten value card) the dealer wins.");
         System.out.println("If the dealer goes bust then everyone else wins.");
         System.out.println("We reshuffle the deck of cards after every game.");
-        System.out.println();
+
     }//end instructions
 
 }//end class
